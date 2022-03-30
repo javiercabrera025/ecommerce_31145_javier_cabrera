40ff21f7d5c33377bd322ce2fc4a36f7a9c4123f
Javier Cabrera <javier.cabrera@wundermanthompson.com>
2022-03-30 04:14:27 -0300

    item list container

---
+++ src/components/itemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+import "./itemListContainer.css";
+
+const ItemListContainer = (props) => {
+    return (
+        <h1 class="title">{props.greetings}</h1>
+    )
+}
+
+export default ItemListContainer;
