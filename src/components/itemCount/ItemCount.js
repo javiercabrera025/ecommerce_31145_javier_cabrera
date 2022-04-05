04308eeb6a49e9e9d916b1828bdc2404fdb522c3
Javier Cabrera <javier.cabrera@wundermanthompson.com>
2022-04-04 22:29:06 -0300

    add initial to useState. Add condition to onAdd callback

--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const ItemCount = ({ initial, stock, onAdd }) => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(initial);
 
   const decrement = () => {
     if (count > initial) {
@@ -55,7 +55,7 @@ const ItemCount = ({ initial, stock, onAdd }) => {
             </button>
           </div>
           <button
-            onClick={() => onAdd(count)}
+            onClick={ count>0 ? () => onAdd(count) : undefined }
             className="mt-10 w-full bg-gray-900 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Agregar al carrito
