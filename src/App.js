c06721186479d91db2236d602a2db1df04eba250
Javier Cabrera <javier.cabrera@wundermanthompson.com>
2022-04-12 10:04:46 -0300

    routing sin terminar, faltan filtrar categorias

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./assets/css/styles.css";
 import NavBar from "./components/navbar/NavBar";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greetings={'This is Gaming House'} />
-      <ItemDetailContainer/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/detail/:productId" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1>NOT FOUND 404</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/itemDetailContainer/ItemDetailContainer.js
+++ src/components/itemDetailContainer/ItemDetailContainer.js
@@ -1,24 +1,42 @@
 import { useEffect, useState } from "react";
 import { getProductsById } from "../../asyncmock";
 import ItemDetail from "../itemDetail/ItemDetail";
+import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
-  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const { productId } = useParams();
+
   useEffect(() => {
-    getProductsById(1)
+    getProductsById(productId)
       .then((item) => {
-        setProducts(item);
+        setProduct(item);
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  }, []);
+
+    return () => {
+      setProduct();
+    };
+  }, [productId]);
 
   return (
     <div>
       <section>
         <div className="container mx-auto pl-5 pr-5">
-          <ItemDetail {...products} />
+          {loading ? (
+            <h1>Cargando...</h1>
+          ) : product ? (
+            <ItemDetail {...product} />
+          ) : (
+            <h1>El producto no existe</h1>
+          )}
         </div>
       </section>
     </div>
